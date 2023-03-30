84156fac21703a262500c8b9a82b0867c174a13d
Mohammad Prince <mprince2k16@gmail.com>
2023-03-30 14:48:22 +0600

    main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import fitz
+# specify the path of the PDF file
+pdf_path = "2021.pdf"
+# Open the PDF
+try:
+    pdf = fitz.open(pdf_path)
+except FileNotFoundError:
+    print("File not found")
+# Iterate over the pages starting from the 6th page
+for i, page in enumerate(pdf):
+    try:
+        # Render the page as a JPEG image
+        pix = page.get_pixmap(alpha=False)
+        # Save the image
+        pix.writeJPG(f"page_{i+1}.jpg")
+    except Exception as e:
+        print(e)
+# Close the PDF
+try:
+    pdf.close()
+    print("Successful")
+except Exception as e:
+    print(e)
